8ca6276761a6d690dbdfd274cae9297b04a241ee
Đông Vũ <107346302+Dong0711@users.noreply.github.com>
2023-11-05 21:39:34 +0700

    push a list farm camera page

---
+++ farm/lib/page/ListFarmPage.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+
+import 'package:farm/components/FarmInfoContainer.dart';
+import 'package:farm/components/Mytext.dart';
+import 'package:farm/components/inforCamera.dart';
+import 'package:farm/config/theme/AppColor.dart';
+import 'package:farm/model/farm.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ListFarmPage extends StatefulWidget {
+  const ListFarmPage({super.key});
+
+  @override
+  State<ListFarmPage> createState() => _ListFarmPageState();
+}
+
+class _ListFarmPageState extends State<ListFarmPage> {
+  List<FarmInfoContainer> listFarm = [];
+  Future GetFarms() async {
+    var response = await http
+        .get(Uri.parse('https://fake-api-smart-farm-zwq6.vercel.app/farm'));
+    print(response);
+    if (response.statusCode == 200) {
+      var jsonData = jsonDecode(response.body);
+      for (var item in jsonData) {
+        final farm = Farm.fromJson(item);
+        FarmInfoContainer newFarm = FarmInfoContainer(farm: farm);
+        listFarm.add(newFarm);
+      }
+    } else {
+      throw Exception("Fail to load farm data");
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    GetFarms();
+    // listFarm=FetchFarmInfo()
+  }
+
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            height: screenHeight * 0.15,
+            decoration: BoxDecoration(gradient: AppColor.gradien[50]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const MyText(
+                  text: "QUẢN LÝ CAMERA CỦA TRANG TRẠI",
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ],
+            ),
+          ),
+          // listFarm[0],
+          FutureBuilder(
+            future: GetFarms(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Column(
+                  children: [...listFarm],
+                );
+              } else
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
